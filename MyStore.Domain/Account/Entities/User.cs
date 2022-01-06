40394de639afec3fa68a012cfedc26f468479d2a
Renan Bernardo <renan.brnrd@gmail.com>
2022-01-06 19:24:23 -0300

    Inicia desenvolvimento da entidade User

---
+++ MyStore.Domain/Account/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Domain.Account.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Verified { get; set; }
+        public bool Active { get; set; }
+        public DateTime dateTime { get; set; }
+        public int Role { get; set; }
+        public string VerificationCode { get; set; }
+        public string ActivationCode { get; set; }
+        public string AuthorizationCode { get; set; }
+        public DateTime LastAuthorizationCodeRequest { get; set; }
+    }
+}
