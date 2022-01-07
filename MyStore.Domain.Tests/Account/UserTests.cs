ea908ed4c93d38ea902b5fd48413df7290efd6d2
Renan Bernardo <renan.brnrd@gmail.com>
2022-01-07 08:58:03 -0300

    Cria projeto de Testes do Domain

---
+++ MyStore.Domain.Tests/Account/UserTests.cs
@@ -0,0 +1,21 @@
+﻿using MyStore.Domain.Account.Entities;
+using Xunit;
+
+namespace MyStore.Domain.Tests.Account
+{
+    public class UserTests
+    {
+        [Fact]
+        public void CanActivateUserTest()
+        {
+            var user = new User();
+
+            user.Active = true;
+
+            if (user.Verified)
+            {
+                // user Repository.Save(user);
+            }
+        }
+    }
+}
--- MyStore.Domain/Account/Entities/User.cs
+++ MyStore.Domain/Account/Entities/User.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MyStore.Domain.Account.Entities;
 
-namespace MyStore.Domain.Account.Entities
+public class User
 {
-    public class User
-    {
-        public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public bool Verified { get; set; }
-        public bool Active { get; set; }
-        public DateTime dateTime { get; set; }
-        public int Role { get; set; }
-        public string VerificationCode { get; set; }
-        public string ActivationCode { get; set; }
-        public string AuthorizationCode { get; set; }
-        public DateTime LastAuthorizationCodeRequest { get; set; }
-    }
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public bool Verified { get; set; }
+    public bool Active { get; set; }
+    public DateTime dateTime { get; set; }
+    public int Role { get; set; }
+    public string VerificationCode { get; set; }
+    public string ActivationCode { get; set; }
+    public string AuthorizationCode { get; set; }
+    public DateTime LastAuthorizationCodeRequest { get; set; }
 }
+
