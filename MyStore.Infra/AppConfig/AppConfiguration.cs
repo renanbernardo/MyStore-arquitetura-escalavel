735bb755767eb1901cf34edc0cd9e86391766fb6
Renan Bernardo <renan.brnrd@gmail.com>
2022-02-22 09:32:13 -0300

    Add Infra e AppSettings Configuration

---
+++ MyStore.Infra/AppConfig/AppConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MyStore.Infra.AppConfig
+{
+    public class AppConfiguration
+    {
+        public readonly string _connectionStringMyStore = string.Empty;
+        public AppConfiguration()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+            var root = configurationBuilder.Build();
+            _connectionStringMyStore = root.GetSection("ConnectionString").GetSection("MyStoreConnectionString").Value;
+            var appSetting = root.GetSection("ApplicationSettings");
+        }
+
+        public string ConnectionStringMyStore
+        {
+            get => _connectionStringMyStore;
+        }
+    }
+}
---
+++ MyStore.Infra/Persistence/Contexts/MyStoreDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyStore.Infra.Persistence.Contexts
+{
+    public class MyStoreDataContext : DbContext
+    {
+        public MyStoreDataContext() : base()
+        {
+        }
+    }
+}
