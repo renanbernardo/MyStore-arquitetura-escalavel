29954551a702f3d4e2637ace4ea65a87f5ba2cbf
Renan Bernardo <renan.brnrd@gmail.com>
2022-03-06 11:58:45 -0300

    Iniciando a Application Service

---
+++ MyStore.ApplicationService/Services/ApplicationService.cs
@@ -0,0 +1,38 @@
+﻿using DomainNotificationHelper;
+using DomainNotificationHelper.Events;
+using MyStore.Infra.Transaction;
+
+namespace MyStore.ApplicationService.Services
+{
+    public class ApplicationService
+    {
+        private readonly IHandler<DomainNotification> _notifications;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ApplicationService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _notifications = DomainEvent.Container.GetService<IHandler<DomainNotification>>();
+        }
+
+        public bool Commit()
+        {
+            if (_notifications.HasNotifications())
+                return false;
+
+            _unitOfWork.Commit();
+            return true;
+        }
+
+        public void Rollback(string message)
+        {
+            DomainEvent.Raise<DomainNotification>(new DomainNotification("BusinessError", message));
+            _unitOfWork.Rollback();
+        }
+
+        public void Rollback()
+        {
+            _unitOfWork.Rollback();
+        }
+    }
+}
