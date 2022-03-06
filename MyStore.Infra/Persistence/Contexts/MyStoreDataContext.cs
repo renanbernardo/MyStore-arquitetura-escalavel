4a36579aec151326fb41b51ffda13a1e6cb476d5
Renan Bernardo <renan.brnrd@gmail.com>
2022-03-06 11:14:13 -0300

    Implementando Repository Pattern

--- MyStore.Infra/Persistence/Contexts/MyStoreDataContext.cs
+++ MyStore.Infra/Persistence/Contexts/MyStoreDataContext.cs
@@ -2,6 +2,7 @@
 using MyStore.Domain.Account.Entities;
 using MyStore.Infra.AppConfig;
 using MyStore.Infra.Persistence.Mappings;
+using System.Diagnostics.CodeAnalysis;
 
 namespace MyStore.Infra.Persistence.Contexts
 {
@@ -14,7 +15,7 @@ namespace MyStore.Infra.Persistence.Contexts
             _myStoreDbConectionString = new AppConfiguration().ConnectionStringMyStore;
         }
 
-        public virtual DbSet<User>? Users { get; set; }
+        public virtual DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
