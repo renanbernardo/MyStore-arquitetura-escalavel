fd4b6cab605d4a80865ca8492bb81bc8855c7aec
Renan Bernardo <renan.brnrd@gmail.com>
2022-03-07 19:08:55 -0300

    Desacoplando o User Interface utilizando Application Services

---
+++ MyStore.ApplicationService/Services/Account/UserApplicationService.cs
@@ -0,0 +1,41 @@
+﻿using DomainNotificationHelper.Events;
+using MyStore.Domain.Account.Commands.UserCommands;
+using MyStore.Domain.Account.Entities;
+using MyStore.Domain.Account.Events.UserEvents;
+using MyStore.Domain.Account.Repositories;
+using MyStore.Domain.Account.Services;
+using MyStore.Infra.Transaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.ApplicationService.Services.Account
+{
+    public class UserApplicationService : ApplicationService, IUserApplicationService
+    {
+        private readonly IUserRepository _repository;
+
+        public UserApplicationService(IUserRepository repository, IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            _repository = repository;
+        }
+
+        public User Register(RegisterUserCommand command)
+        {
+            var user = new User(command.Email, command.Username, command.Password);
+
+            user.Register();
+
+            if(Commit())
+            {
+                DomainEvent.Raise(new OnUserRegisteredEvent(user));
+
+                return user;
+            }
+            
+            return null;
+        }
+    }
+}
---
+++ MyStore.Domain/Account/Commands/UserCommands/RegisterUserCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Domain.Account.Commands.UserCommands
+{
+    public class RegisterUserCommand
+    {
+        public RegisterUserCommand(string email, string username, string password)
+        {
+            Email = email;
+            Username = username;
+            Password = password;
+        }
+
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
