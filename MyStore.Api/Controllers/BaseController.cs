59f7f6c16836c9c0a4bfa3599c7b346892a68331
Renan Bernardo <renan.brnrd@gmail.com>
2022-04-07 20:59:27 -0300

    Cria o projeto API

---
+++ MyStore.Api/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using DomainNotificationHelper;
+using DomainNotificationHelper.Events;
+using System.Net;
+using System.Net.Http;
+
+namespace MyStore.Api.Controllers
+{
+    public class BaseController
+    {
+        public IHandler<DomainNotification> Notifications;
+        public HttpResponseMessage ResponseMessage;
+
+        public BaseController()
+        {
+            Notifications = DomainEvent.Container.GetService<IHandler<DomainNotification>>();
+            ResponseMessage = new HttpResponseMessage();
+        }
+
+        public HttpResponseMessage CreateResponse(HttpStatusCode code, object result)
+        {
+            if (Notifications.HasNotifications())
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            else
+                return new HttpResponseMessage(code);
+        }
+    }
+}
